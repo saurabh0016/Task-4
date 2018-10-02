0fda2fc04ebde8042ede9766a8e850e8e161b213
saurabh0016 <saurabh.0016@gmail.com>
2018-10-02 20:16:56 +0530

    Add files via upload

---
+++ Task 4/index.js
@@ -0,0 +1,62 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Hello " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+function saveDetails(form) {
+
+  var json = {
+
+    "date":form.date.value,
+    "qualification":form.qualification.value,
+    "job_details":form.job_details.value,
+    "topics_interest":form.topics_interest.value
+
+  };
+
+  var html = JSON.stringify(json,0,4);
+  document.getElementById('output').innerHTML=html;
+  return false;
+
+}
